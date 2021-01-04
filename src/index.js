28582bcf4b1d032466ae552b15f533a7dbaa2c44
februar-y <jarmah241@gmail.com>
2021-01-04 10:07:38 -0500

    adds movie card render

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ const MOVIES_URL = `${BASE_URL}/movies`
 
 let allMovies = []
 
+// DOM Elements
+const main = document.querySelector("main")
+
 //******  Functions  ******//
 
 const renderMovieCard = (movieObj) => {
     console.log(movieObj)
+    let cardDiv = document.createElement("div")
+        cardDiv.classList.add("card")
+        cardDiv.dataset.userId = 1 //will be set by cookie when user logs in
+    let img = document.createElement("img")
+        img.src = movieObj.image
     let head = document.createElement('h3')
-    // artisnal 
+        head.textContent = movieObj.title
+    let movieInfoDiv = document.createElement("div")
+        movieInfoDiv.classList.add("movie-info")
+    let pInfo = document.createElement("p")
+        pInfo.textContent = movieObj.synopsis
+
+    movieInfoDiv.append(pInfo)
+    cardDiv.append(head,movieInfoDiv)
+    main.append(cardDiv)
 }
 
 
